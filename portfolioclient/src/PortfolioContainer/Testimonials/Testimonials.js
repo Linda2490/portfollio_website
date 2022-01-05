483ed2e845d1420788a63f25099f7dcb185d77a9
Your Name <lindazoghlami@gmail.com>
2022-01-05 05:02:44 +0100

    testimonials-first-part

--- portfolioclient/src/PortfolioContainer/Testimonials/Testimonials.js
+++ portfolioclient/src/PortfolioContainer/Testimonials/Testimonials.js
@@ -10,6 +10,61 @@ export default function Testimonials() {
         </div>
         <div className="testimonials-subtitle">OUR SERVICES FOR CLIENTS</div>
       </div>
+      <div className="testimonials-box">
+        <i class="fas fa-chevron-left"></i>
+        <div className="box">
+          <div className="box-details">
+            <div className="round-photo"></div>
+            <h6>Tech Pro</h6>
+            <h3>Showtag.tv</h3>
+            <h6 style={{ marginTop: '-10px' }}>
+              Timely good quality Wordpress Work
+            </h6>
+            <div className="rating">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="box-details">
+            <div className="round-photo"></div>
+            <h6>Tech Pro</h6>
+            <h3>Showtag.tv</h3>
+            <h6 style={{ marginTop: '-10px' }}>
+              Timely good quality Wordpress Work
+            </h6>
+            <div className="rating">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </div>
+          </div>
+        </div>
+        <div className="box">
+          <div className="box-details">
+            <div className="round-photo"></div>
+            <h6>Tech Pro</h6>
+            <h3>Showtag.tv</h3>
+            <h6 style={{ marginTop: '-10px' }}>
+              Timely good quality Wordpress Work
+            </h6>
+            <div className="rating">
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+            </div>
+          </div>
+        </div>
+        <i class="fas fa-chevron-right"></i>
+      </div>
     </div>
   )
 }
