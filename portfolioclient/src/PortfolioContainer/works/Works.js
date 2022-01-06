15c8ed77d1c34d4e50efce091727f1ba201def2e
Your Name <lindazoghlami@gmail.com>
2022-01-07 00:09:37 +0100

    works-component finished

--- portfolioclient/src/PortfolioContainer/works/Works.js
+++ portfolioclient/src/PortfolioContainer/works/Works.js
@@ -11,6 +11,65 @@ export default function Works() {
         </div>
         <div className="works-subtitle">THINGS WE'VE MADE</div>
       </div>
+      <div className="works-details">
+        <div className="showtag">
+          <div className="first-pic"></div>
+
+          <div className="first-pic-details">
+            <div className="first-pic-title">Showtag.tv</div>
+            <div className="underline"></div>
+            <div className="first-pic-description">
+              We develop web applications from landing pages to fully E-commerce
+              websites We develop web applications from landing pages to fully
+              E-commerce websitesWe develop web applications from landing pages
+              to fully E-commerce websitesWe develop web applications from
+              landing pages to fully E-commerce websites
+            </div>
+            <button style={{ margin: '95px', marginTop: '240px' }}>
+              view the website
+            </button>
+          </div>
+        </div>
+
+        <div className="showtag">
+          <div className="second-pic"></div>
+
+          <div className="first-pic-details">
+            <div className="second-pic-title">bombshellsa.com</div>
+            <div className="underline"></div>
+            <div className="first-pic-description">
+              We develop web applications from landing pages to fully E-commerce
+              websites We develop web applications from landing pages to fully
+              E-commerce websitesWe develop web applications from landing pages
+              to fully E-commerce websitesWe develop web applications from
+              landing pages to fully E-commerce websites
+            </div>
+            <button style={{ margin: '95px', marginTop: '240px' }}>
+              view the website
+            </button>
+          </div>
+        </div>
+        <div className="showtag">
+          <div className="third-pic"></div>
+
+          <div className="first-pic-details">
+            <div className="third-pic-title">agencyone.fr</div>
+            <div className="underline"></div>
+            <div className="first-pic-description">
+              We develop web applications from landing pages to fully E-commerce
+              websites We develop web applications from landing pages to fully
+              E-commerce websitesWe develop web applications from landing pages
+              to fully E-commerce websitesWe develop web applications from
+              landing pages to fully E-commerce websites
+            </div>
+            <button style={{ margin: '95px', marginTop: '240px' }}>
+              view the website
+            </button>
+          </div>
+        </div>
+        <button style={{marginLeft:'450px', marginTop:'1190px'}}>view more works</button>
+      </div>
+      
     </div>
   )
 }
