910e6299dcbf3e6cb23752bac90de402e2ba971d
Your Name <lindazoghlami@gmail.com>
2022-01-21 04:34:30 +0100

    adjustments

--- portfolioclient/src/PortfolioContainer/Testimonials/Testimonials.js
+++ portfolioclient/src/PortfolioContainer/Testimonials/Testimonials.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Testimonials.css'
 export default function Testimonials() {
   return (
-    <div className="testimonials-container" id='testimonials'>
+    <div className="testimonials-container" id="testimonials">
       <div className="testimonials-parent">
         <div className="row">
           <div className="testimonials-line"></div>
@@ -10,98 +10,98 @@ export default function Testimonials() {
         </div>
         <div className="testimonials-subtitle">OUR SERVICES FOR CLIENTS</div>
       </div>
-      
+
       <div className="testimonials-box">
-        <i class="fas fa-chevron-left" ></i>
-        <div className='sliderr'>
-        <div className='slidess'>
-        <div className="box">
-          <div className="box-details">
-            <div className="round-photo"></div>
-            <h6 className='h6'>Tech Pro</h6>
-            <h3 className='h3'>Showtag.tv</h3>
-            <h6 style={{ marginTop: '-10px' }}>
-              Timely good quality Wordpress Work
-            </h6>
-            <div className="rating">
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
+        <i class="fas fa-chevron-left"></i>
+        <div className="sliderr">
+          <div className="slidess">
+            <div className="box">
+              <div className="box-details">
+                <div className="round-photo"></div>
+                <h6 className="h6">Tech Pro</h6>
+                <h3 className="h3">Showtag.tv</h3>
+                <h6 style={{ marginTop: '-10px' }}>
+                  Timely good quality Wordpress Work
+                </h6>
+                <div className="rating">
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                </div>
+              </div>
+            </div>
+
+            <div className="box">
+              <div className="box-details">
+                <div className="round-photo"></div>
+                <h6 className="h6">Tech Pro</h6>
+                <h3 className="h3">Showtag.tv</h3>
+                <h6 style={{ marginTop: '-10px' }}>
+                  Timely good quality Wordpress Work
+                </h6>
+                <div className="rating">
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                </div>
+              </div>
+            </div>
+            <div className="box">
+              <div className="box-details">
+                <div className="round-photo"></div>
+                <h6 className="h6">Tech Pro</h6>
+                <h3 className="h3">Showtag.tv</h3>
+                <h6 style={{ marginTop: '-10px' }}>
+                  Timely good quality Wordpress Work
+                </h6>
+                <div className="rating">
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                </div>
+              </div>
+            </div>
+            <div className="box">
+              <div className="box-details">
+                <div className="round-photo"></div>
+                <h6 className="h6">Tech Pro</h6>
+                <h3 className="h3">Showtag.tv</h3>
+                <h6 style={{ marginTop: '-10px' }}>
+                  Timely good quality Wordpress Work
+                </h6>
+                <div className="rating">
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                </div>
+              </div>
+            </div>
+            <div className="box">
+              <div className="box-details">
+                <div className="round-photo"></div>
+                <h6 className="h6">Tech Pro</h6>
+                <h3 className="h3">Showtag.tv</h3>
+                <h6 style={{ marginTop: '-10px' }}>
+                  Timely good quality Wordpress Work
+                </h6>
+                <div className="rating">
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                  <i className="fas fa-star"></i>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        
-        <div className="box">
-          <div className="box-details">
-            <div className="round-photo"></div>
-            <h6 className='h6'>Tech Pro</h6>
-            <h3 className='h3'>Showtag.tv</h3>
-            <h6 style={{ marginTop: '-10px' }}>
-              Timely good quality Wordpress Work
-            </h6>
-            <div className="rating">
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-            </div>
-          </div>
-        </div>
-        <div className="box">
-          <div className="box-details">
-            <div className="round-photo"></div>
-            <h6 className='h6'>Tech Pro</h6>
-            <h3 className='h3'>Showtag.tv</h3>
-            <h6 style={{ marginTop: '-10px' }}>
-              Timely good quality Wordpress Work
-            </h6>
-            <div className="rating">
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-            </div>
-          </div>
-        </div>
-        <div className="box">
-          <div className="box-details">
-            <div className="round-photo"></div>
-            <h6 className='h6'>Tech Pro</h6>
-            <h3 className='h3'>Showtag.tv</h3>
-            <h6 style={{ marginTop: '-10px' }}>
-              Timely good quality Wordpress Work
-            </h6>
-            <div className="rating">
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-            </div>
-          </div>
-        </div>
-        <div className="box">
-          <div className="box-details">
-            <div className="round-photo"></div>
-            <h6 className='h6'>Tech Pro</h6>
-            <h3 className='h3'>Showtag.tv</h3>
-            <h6 style={{ marginTop: '-10px' }}>
-              Timely good quality Wordpress Work
-            </h6>
-            <div className="rating">
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-              <i class="fas fa-star"></i>
-            </div>
-          </div>
-          </div>
-        </div>
         </div>
         <i class="fas fa-chevron-right"></i>
       </div>
